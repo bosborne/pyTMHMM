6e78c552bc3b23cb6bd9d9e76696729aa3f08751
Brian Osborne <briano@bioteam.net>
2020-12-14 10:51:14 -0500

    Add classifiers to setup

--- setup.py
+++ setup.py
@@ -6,28 +6,23 @@ setup(
     name='tmhmm.py',
     version='1.3.2',
     author='Brian Osborne',
-    author_email='briano@bioteam.net',
+    author_email='bosborne@alum.mit.edu',
     description='A transmembrane helix finder.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-
     url='https://github.com/bosborne/tmhmm.py/',
     packages=['tmhmm'],
     package_data={'tmhmm': ['TMHMM2.0.model']},
     zip_safe=False,
-
     python_version='>=3.5',
-
     setup_requires=['setuptools>=18.0', 'numpy>=1.9', 'cython'],
     install_requires=['numpy>=1.9'],
     extras_require={
         'plotting':  ['matplotlib'],
     },
-
     entry_points={
         'console_scripts': ['tmhmm=tmhmm.cli:cli'],
     },
-
     ext_modules=[
         Extension(
             'tmhmm.hmm',
@@ -35,4 +30,9 @@ setup(
             include_dirs=[numpy.get_include()],
         ),
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: Apache Software License"
+    ]
 )
